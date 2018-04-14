b76e0d0b7fa2bcbd23c8a70a6712eca07aeb9f85
Andre Marques <andre.acm97@outlook.com>
2018-04-14 16:11:16 -0300

    Mudanças no formato da saída

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,11 @@
 import br.unb.cic.epl.Core
 import br.unb.cic.epl.Eval
 import br.unb.cic.epl.Add
-import br.unb.cic.epl.AddEval
 import br.unb.cic.epl.Sub
 import br.unb.cic.epl.Mult
+import br.unb.cic.epl.AddEval
+import br.unb.cic.epl.SubEval
+import br.unb.cic.epl.MultEval
 
 object Main extends App {
   val lit100 = new Core.Literal(100) with Eval.Literal
@@ -16,11 +18,14 @@ object Main extends App {
   val sum = new Add.Add(lit100, lit500)
   val sub = new Sub.Sub(lit900, lit250)
   val mult = new Mult.Mult(lit5, lit10)
+  val sumeval = new AddEval.Add(lit100, lit500)
+  val subeval = new SubEval.Sub(lit900, lit250)
+  val multeval = new MultEval.Mult(lit5, lit10)
 
   println(sum.print())
-  println(lit100.eval())
+  println(sumeval.eval())
   println(sub.print())
-  println(lit900.eval())
+  println(subeval.eval())
   println(mult.print())
-  println(lit5.eval())
+  println(multeval.eval())
 }
